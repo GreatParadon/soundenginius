eed43a27221f517e03ccab1add7e1790d504ff93
Paradon Wisdomlanna <paradon@wisdomlanna.com>
2017-02-06 09:15:12 +0700

    - Edit margin for banner

--- resources/views/web_component/footer.blade.php
+++ resources/views/web_component/footer.blade.php
@@ -1,30 +1,37 @@
 <style>
     footer {
         border-top: 1px solid #E0E0E0;
-        color: #999999;
+
     }
 
-    #f_social {
-        margin-top: 10px;
+    .foot-edit {
+        text-align: center;
+        width: 90%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .footer1 {
+        text-align: left;
+    }
+
+    .f_social {
+        margin-top: 20px;
         float: right;
     }
 
-    #f_social img {
-        margin-left: 10px;
-    }
 </style>
-<footer class="container-fluid">
-    <div class="row">
-        <div class="col-md-6">
+<footer>
+    <div class="row container foot-edit">
+        <div class="col-md-6 footer1">
             <p><a href="/">หน้าแรก</a> | <a href="/contact">ติดต่อเรา</a>
-
-            <p id="f_info">
+            <p>
                 188/1 หมู่ 10 ตำบล สารภี อำเภอ สารภี จังหวัด เชียงใหม่ 50140<br>
                 โทร: 081-8842022, 082-3820505
             </p>
         </div>
         <div class="col-md-6">
-            <div id="f_social">
+            <div class="f_social">
                 <a href="https://www.facebook.com/soundenginius"><img
                             src="{{ asset('resource/footer/facebook-icon.png') }}" width="50"> SOUNDENGINIUS</a>
             </div>
